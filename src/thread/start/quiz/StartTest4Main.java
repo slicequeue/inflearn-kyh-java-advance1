a184f7a8d94428336a3127e7abd2bfab8b088847
slicequeue <slicequeue@gmail.com>
2025-12-22 21:41:10 +0900

    3-16 문제와 풀이 - test4 보강

--- src/thread/start/quiz/StartTest4Main.java
+++ src/thread/start/quiz/StartTest4Main.java
@@ -7,38 +7,34 @@ public class StartTest4Main {
     public static void main(String[] args) {
 
         log("start");
-        Thread thread1 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    log("A");
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        });
+        Thread thread1 = new Thread(new PrintWork("A", 1000));
+        Thread thread2 = new Thread(new PrintWork("B", 500));
 
-        Thread thread2 = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (true) {
-                    log("B");
-                    try {
-                        Thread.sleep(500);
-                    } catch (InterruptedException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-            }
-        });
-        thread1.setName("Thread-A");
         thread1.start();
-
-        thread2.setName("Thread-B");
         thread2.start();
         log("end");
+    }
+
+    static class PrintWork implements Runnable {
+
+        private String content;
+        private int sleepMs;
+
+        public PrintWork(String content, int sleepMs) {
+            this.content = content;
+            this.sleepMs = sleepMs;
+        }
+
+        @Override
+        public void run() {
+            while (true) {
+                log(content);
+                try {
+                    Thread.sleep(sleepMs);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
     }
 }
